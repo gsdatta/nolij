66b1887587e5b4e99b4eacf95e4c9a2cb398c10a
Aditya Subramanian <subrmnn4@illinois.edu>
2015-12-29 21:03:58 -0800

    stupid fix

--- nolij/company/forms.py
+++ nolij/company/forms.py
@@ -6,7 +6,7 @@ from nolij.company.models import Company
 class CompanyForm(Form):
     name = StringField('Company Name', validators=[validators.input_required()])
     domain = StringField('Company Domain', validators=[validators.input_required()])
-    email
+    email = StringField('Company Email', validators = [validators.input_required()])
     def validate_domain(form, field):
         company = Company.query.filter_by(domain=field.data).first()
         if company is not None:
